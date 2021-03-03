d25e44a9eb20febbee1795c950abad67cb0ad7cd
Mert <mertuygur02@gmail.com>
2021-03-03 21:27:19 +0300

    layout

--- client/src/components/Questionaire.js
+++ client/src/components/Questionaire.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Questionaire = ({ data }) => {
 
+    const [currentQuestion, setCurrentQuestion] = useState(data[0]);
+    const [questionIndex, setQuestionIndex] = useState(0);
+
+    const handleClick = () => {
+        if (data[questionIndex]) {
+            setQuestionIndex(questionIndex + 1);
+            setCurrentQuestion(data[questionIndex])
+        }
+        else {
+            return false;
+        }
+
+    }
     return (
         <div className="quest">
             <div className="quest__number">
-                <p>QUESTION 1</p>
+                <p>QUESTION {questionIndex + 1} / {data.length + 1}</p>
             </div>
             <div className="quest__question">
-                <h1>{data[0]}</h1>
+                <h1>{currentQuestion}</h1>
             </div>
 
             <div className="quest__options">
-                <button className="zero"></button>
-                <button className="one"></button>
-                <button className="two"></button>
-                <button className="three"></button>
-                <button className="four"></button>
+                <button className="zero" onClick={() => { handleClick() }}></button>
+                <button className="one" onClick={() => { handleClick() }}></button>
+                <button className="two" onClick={() => { handleClick() }}></button>
+                <button className="three" onClick={() => { handleClick() }}></button>
+                <button className="four" onClick={() => { handleClick() }}></button>
             </div>
 
         </div>
